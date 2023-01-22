83c33e39ee4a298449672d6d578ce154606ea6f7
Lucas <lucasmatiasblanco@outlook.com>
2023-01-21 21:11:33 -0300

    more fixes to github pages

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
        />
      </div>
      <Routes>
-       <Route path="/" element={<Home />} />
+       <Route path="./" element={<Home />} />
        <Route
          path="/shop"
          element={
